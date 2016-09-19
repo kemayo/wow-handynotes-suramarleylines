13fedee3b645743714cae85880a641e3720e2c6c
David Lynch <kemayo@gmail.com>
2016-09-19 16:28:23 -0500

    Initial commit

---
+++ nodes.lua
@@ -0,0 +1,14 @@
+local myname, ns = ...
+
+ns.points = {
+    ["Suramar"] = {
+        [41703890] = { achievement=10756, criteria=31056, }, -- Anora Hollow (the prerequisite!)
+        [29008480] = { achievement=10756, criteria=31918, }, -- Soul Vaults
+        [59304280] = { achievement=10756, criteria=31914, }, -- Kel'balor
+        [65804190] = { achievement=10756, criteria=31913, }, -- Elor'shan
+        [20405040] = { achievement=10756, criteria=31917, }, -- Falanar South
+        [21404330] = { achievement=10756, criteria=31916, }, -- Falanar North
+        [24301940] = { achievement=10756, criteria=31919, }, -- Moon Guard
+        [35702410] = { achievement=10756, criteria=31915, }, -- Moonwhisper Gulch
+    },
+}
