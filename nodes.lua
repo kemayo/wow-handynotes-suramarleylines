d25c0ea467fb7dd3b679a8f6e68d275fb8660b34
David Lynch <kemayo@gmail.com>
2019-04-19 18:19:36 -0500

    Update for uiMapId

--- nodes.lua
+++ nodes.lua
@@ -1,7 +1,7 @@
 local myname, ns = ...
 
 ns.points = {
-    ["Suramar"] = {
+    [680] = { -- Suramar
         [41703890] = { achievement=10756, criteria=31056, }, -- Anora Hollow (the prerequisite!)
         [29008480] = { achievement=10756, criteria=31918, }, -- Soul Vaults
         [59304280] = { achievement=10756, criteria=31914, }, -- Kel'balor
@@ -11,20 +11,20 @@ ns.points = {
         [24301940] = { achievement=10756, criteria=31919, }, -- Moon Guard
         [35702410] = { achievement=10756, criteria=31915, }, -- Moonwhisper Gulch
     },
-    ["FalanaarTunnels"] = { -- Fal'adore
-        [65105210] = { achievement=10756, criteria=31916, level=33, }, -- Falanaar North
-        [58107520] = { achievement=10756, criteria=31917, level=33, }, -- Falanaar South
+    [685] = { -- Falanaar Tunnels
+        [65105210] = { achievement=10756, criteria=31916, }, -- Falanaar North
+        [58107520] = { achievement=10756, criteria=31917, }, -- Falanaar South
     },
-    ["LeylineMicro01"] = {
-        [46704720] = { achievement=10756, criteria=31913, }, -- Elor'shan
+    [686] = { -- Elor'shan
+        [46704720] = { achievement=10756, criteria=31913, },
     },
-    ["LeylineMicro02"] = {
-        [52304490] = { achievement=10756, criteria=31914, }, -- Kel'balor
+    [687] = { -- Kel'balor
+        [52304490] = { achievement=10756, criteria=31914, },
     },
-    ["LeylineMicro04"] = {
-        [54004470] = { achievement=10756, criteria=31915, }, -- Moonwhisper Gulch
+    [689] = { -- Ley Station Moonfall, Moonwhisper Gulch
+        [54004470] = { achievement=10756, criteria=31915, },
     },
-    ["LeylineMicro05"] = {
-        [48704870] = { achievement=10756, criteria=31919, }, -- Moon Guard
+    [690] = { -- Ley Station Aethenar, Moon Guard
+        [48704870] = { achievement=10756, criteria=31919, },
     },
 }
